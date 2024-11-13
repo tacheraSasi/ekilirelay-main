9a821d3d12b39c5d3ff51beea2c0739fab5d5005
tacheraSasi <tacherasasi@gmail.com>
2024-11-13 20:04:58 +0300

    finished the default template

--- api/configurations.php
+++ api/configurations.php
@@ -1,8 +1,17 @@
 <?php 
 #The default template function
-
-function defaultTemplate():string{
-    $template = '';
+function defaultTemplate(string $message):string{
+    $template = '
+        <!DOCTYPE html>
+        <html lang="en">
+        <body style="font-family: system-ui, -apple-system, BlinkMacSystemFont, Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;">
+            <pre style="font-family: inherit;">
+                '.$message.'
+            </pre>
+            <small style="opacity: .7;">Sent with <a href="http://relay.ekilie.com" style="color:rgb(4, 189, 96)">ekiliRelay</a></small>
+        </body>
+        </html>
+    ';
     return $template;
 
 }
--- api/index.php
+++ api/index.php
@@ -152,4 +152,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo json_encode($response);
     logMessage(json_encode($response));
 }
-?>
+
