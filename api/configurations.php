4b91b74cb25c6b298bc9f0f12f7cb8da19a6574f
tacheraSasi <tacherasasi@gmail.com>
2024-12-23 21:40:48 +0300

    template

--- api/configurations.php
+++ api/configurations.php
@@ -3,7 +3,29 @@
 function defaultTemplate(string $message):string{
     $template = '
         <!DOCTYPE html>
-        <html lang="en">
+        <html lang="en"><head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Email Template</title>
+            <style>
+                body {
+                    font-family: system-ui, -apple-system, BlinkMacSystemFont, Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
+                }
+
+                pre {
+                    font-family: inherit;
+                }
+
+                small {
+                    opacity: .7;
+                }
+
+                a {
+                    color: rgb(4, 189, 96);
+                }
+            </style>
+        </head>
         <body style="font-family: system-ui, -apple-system, BlinkMacSystemFont, Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;">
             <pre style="font-family: inherit;">
                 '.$message.'
