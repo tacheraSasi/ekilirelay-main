9960d3574b256b19fc56031b29faface0de4e33b
tacheraSasi <tacherasasi@gmail.com>
2024-12-26 23:39:45 +0300

    generating the otp

--- auth/reset/php/reset.php
+++ auth/reset/php/reset.php
@@ -1,19 +1,29 @@
 <?php
 session_start();
-require '../../../vendor/autoload.php';
+require "../../../vendor/autoload.php";
 include_once "../../../config.php";
 
+$email = mysqli_real_escape_string($conn, $_POST["email"]);
 
-$email = mysqli_real_escape_string($conn, $_POST['email']);
-
-if(!empty($email)){
+if (!empty($email)) {
     $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
 
-    if(mysqli_num_rows($sql) > 0){
+    if (mysqli_num_rows($sql) > 0) {
         $row = mysqli_fetch_assoc($sql);
 
-        $otp = generateOTP($conn,$email);
-        // Send the OTP to the user's email for password reset logic
+        $otp = generateOTP($conn, $email);
+        // Sending the OTP to the user's email for password reset logic
+
+        if (
+            mail(
+                $email,
+                "Password Reset -> ekiliRelay",
+                "Your OTP is $otp",
+                "From:ekiliRelay <support@ekilie.com>"
+            )
+        ) {
+            echo "success";
+        }
     } else {
         echo "No account was found with email $email";
     }
@@ -21,11 +31,19 @@ if(!empty($email)){
     echo "Email is required";
 }
 
-function generateOTP($conn,$email){
-    $prefix = 'ekilie-';
+function generateOTP($conn, $email){
+    $prefix = "ekilie-";
     $length = 16 - strlen($prefix);
     $otp = $prefix . bin2hex(random_bytes($length / 2));
     $sql = "INSERT INTO otp (email,value) VALUES ('$email','$otp')";
-    return $otp;
+    if (mysqli_query($conn, $sql)) {
+        return $otp;
+    } else {
+        echo "Something went wrong. Please try again later";
+        return $otp;
+    }
+}
+
+function emailTemplate(){
 }
 ?>
