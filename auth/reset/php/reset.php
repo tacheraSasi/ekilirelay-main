4a0e1be213de26d4f92c5a1f19be35c874d0d427
Tachera Sasi <tacherasasi@gmail.com>
2025-01-02 13:13:21 +0300

    expire date

--- auth/reset/php/reset.php
+++ auth/reset/php/reset.php
@@ -12,6 +12,9 @@ if (!empty($email)) {
         $row = mysqli_fetch_assoc($sql);
 
         $otp = generateOTP($conn, $email);
+        if ($otp == "exists") {
+            echo "Check you emails for the reset password link";
+        }
         // Sending the OTP to the user's email for password reset logic
 
         if (
@@ -35,16 +38,18 @@ function generateOTP($conn, $email){
     #checking if the email already has an OTP
     $sql = mysqli_query($conn, "SELECT * FROM otp WHERE email = '{$email}'");
     if (mysqli_num_rows($sql) > 0) {
-        $row = mysqli_fetch_assoc($sql);
-        return "Check you emails for the reset password link";
-    }// Also delete the previous OTP if it has expires
-    
+        // $row = mysqli_fetch_assoc($sql);
+        return "exists";
+    } // Also delete the previous OTP if it has expires
+
     $prefix = "otp_";
     $length = 10 - strlen($prefix);
     $otp = $prefix . bin2hex(random_bytes($length / 2));
-    $expires = strtotime("+7 days",time());
+    $currentTimestamp = time();
+    $expires = strtotime("+7 days", $currentTimestamp);
+    $readableDate = date("Y-m-d H:i:s", $expires);
     $sql = "INSERT INTO otp (email,value,expires_at) VALUES ('$email','$otp',$expires)";
-    
+
     if (mysqli_query($conn, $sql)) {
         return $otp;
     } else {
