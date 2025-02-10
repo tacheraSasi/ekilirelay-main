b496262979b148b151a13e65acbc118ec110ed56
Tachera Sasi <tacherasasi@gmail.com>
2025-02-11 00:52:37 +0300

    Ellie: api class

--- api/api.php
+++ api/api.php
@@ -1,5 +1,11 @@
 <?php
 
+class Api{
+    public static function Header(string $config){
+        Header($config);
+    }
+}
+
 class Method{
     public static function POST(){
         return $_SERVER["REQUEST_METHOD"] == "POST";
--- api/storage/v1/index.php
+++ api/storage/v1/index.php
@@ -1,7 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Content-Type: application/json");
+require_once("../api.php");
+Api::Header("Access-Control-Allow-Origin: *");
+Api::Header("Access-Control-Allow-Methods: POST");
+Api::Header("Content-Type: application/json");
 
 $uploadDir = __DIR__ . '/uploads/';
 $maxFileSize = 100 * 1024 * 1024; // 100MB
