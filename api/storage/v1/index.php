fa05bfd292d826a5955e3e5558fb0e4ee3f9eaac
Tachera Sasi <tacherasasi@gmail.com>
2025-02-11 00:45:39 +0300

    Ellie: more changes

--- api/storage/v1/index.php
+++ api/storage/v1/index.php
@@ -1,1 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json");
+
+$uploadDir = __DIR__ . '/uploads/';
+$maxFileSize = 100 * 1024 * 1024; // 100MB
+$allowedTypes = ['image/jpeg', 'image/png', 'application/pdf'];
